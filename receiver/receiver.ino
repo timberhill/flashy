a0bdb736062abb7b460ea22052c1de5e08c05c46
Max Timberhill <pink.fascist@gmail.com>
2021-05-19 18:42:32 +0100

    made the strip switch off if no data is being sent

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,11 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 8  // pin the LED strip is connected to
-#define LED_NUMBER 1  // number of LEDs in a strip
+#define LED_PIN 6  // pin the LED strip is connected to
+#define LED_NUMBER 15  // number of LEDs in a strip
+#define DIMMING_STEP 1  // dim LEDs by this step when there is no data coming in
+#define MAXIMUM_BRIGHTNESS 100  // 
 
 // initialise the LED strip
+// docs: https://adafruit.github.io/Adafruit_NeoPixel/html/class_adafruit___neo_pixel.html
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUMBER, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+byte led_index = 0;
+byte led_red = 0;
+byte led_green = 0;
+byte led_blue = 0;
+byte led_brightness = MAXIMUM_BRIGHTNESS;
+int unavailable_count = 0;
 
 //////////////////////////////////
 ///////       MAIN        ////////
@@ -13,7 +21,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUMBER, LED_PIN, NEO_GRB + NEO_K
 void setup() {
     // initialise the LED strip
     strip.begin();
-    strip.setBrightness(100);
+    strip.setBrightness(led_brightness);
     strip.show();
     // initialise serial communication
     Serial.begin(9600);
@@ -21,20 +29,23 @@ void setup() {
 
 void loop() {
     while (Serial.available() > 0) {
-        // wait for the starting character
-        char start = Serial.read();
-        if (start != '>') {
-            continue;
-        }
+        if (Serial.read() != '>') continue;  // wait for the starting character
+        unavailable_count = 0;
 
         // starting character encountered, read the next 12 characters
-        byte led_index = readSerialThreeDigitNumber();
-        byte led_red = readSerialThreeDigitNumber();
-        byte led_green = readSerialThreeDigitNumber();
-        byte led_blue = readSerialThreeDigitNumber();
+        led_index = readSerialThreeDigitNumber();
+        led_red = readSerialThreeDigitNumber();
+        led_green = readSerialThreeDigitNumber();
+        led_blue = readSerialThreeDigitNumber();
 
         // update an LED value accordingly
-        updateLedValue(led_index, led_red, led_green, led_blue);
+        updateLEDValue(led_index, led_red, led_green, led_blue);
+    }
+
+    unavailable_count++;
+    delay(10);
+    if (unavailable_count > 100) {
+        switchOffAllLEDs();
     }
 }
 //////////////////////////////////
@@ -49,8 +60,14 @@ byte readSerialThreeDigitNumber() {
 }
 
 
-void updateLedValue(byte i, byte r, byte g, byte b) {
+void updateLEDValue(byte i, byte r, byte g, byte b) {
     // set the value of an LED and refresh the strip
     strip.setPixelColor(i, strip.Color(r,g,b));
+    strip.show();
+}
+
+
+void switchOffAllLEDs() {
+    for (int i = 0; i < LED_NUMBER; i++) updateLEDValue(i, 0, 0, 0);
     strip.show();
 }
