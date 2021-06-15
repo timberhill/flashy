5e492250bcb589aa788838cb73035301620f9010
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2021-06-15 14:08:54 +0100

    moved the screenshot grabber into a separated class

---
+++ transmitter/bitblt_screenshot.py
@@ -0,0 +1,41 @@
+import logging
+from datetime import datetime
+import win32gui, win32ui, win32con
+from PIL import Image
+
+
+class BitBltScreenshot:
+    def __init__(self, bbox, hwnd=None):
+        self.bbox = bbox
+        self.hwnd = hwnd
+        self.size = (self.bbox[2]-self.bbox[0], self.bbox[3]-self.bbox[1])
+        self.position = (self.bbox[0], self.bbox[1])
+        self.logger = logging.getLogger("BitBltScreenshot")
+        self._grab()  # get the screenshot
+    
+    def getpixel(self, *args, **kwargs):
+        return self.screenshot.getpixel(*args, **kwargs)
+    
+    def _grab(self):
+        start = datetime.utcnow()
+        self.hdc = win32gui.GetWindowDC(self.hwnd)
+        self.dc = win32ui.CreateDCFromHandle(self.hdc)
+        self.memdc = self.dc.CreateCompatibleDC()
+        self.bitmap = win32ui.CreateBitmap()
+        self.bitmap.CreateCompatibleBitmap(self.dc, self.size[0], self.size[1])
+        self.memdc.SelectObject(self.bitmap)
+        self.memdc.BitBlt((0, 0), self.size, self.dc, self.position, win32con.SRCCOPY)
+        self.bits = self.bitmap.GetBitmapBits(True)
+        self.screenshot = Image.frombuffer('RGB', self.size, self.bits, 'raw', 'BGRX', 0, 1)
+        grabtime_ms = (datetime.utcnow() - start).total_seconds() * 1000
+        self.logger.debug(f"grabbed a screenshot, duration={grabtime_ms}ms")
+        
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        win32gui.DeleteObject(self.bitmap.GetHandle())
+        self.memdc.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, self.hdc)
+        self.logger.debug(f"exit called with exc_type={exc_type}, " +
+                          f"exc_value={exc_value}, exc_traceback={exc_traceback}")
--- transmitter/screen_reader_async.py
+++ transmitter/screen_reader_async.py
@@ -1,11 +1,11 @@
 import threading
 import logging
 import random
-from ctypes import windll
 import time
 from datetime import datetime
-import win32gui, win32ui, win32con
-from PIL import Image
+
+from .bitblt_screenshot import BitBltScreenshot
+
 
 class ScreenReaderAsync(threading.Thread):
     """Class reading RGB values from the screen in a separate thread.
@@ -13,24 +13,22 @@ class ScreenReaderAsync(threading.Thread):
     https://stackoverflow.com/questions/48092655/memory-leak-with-createdcfromhandle-createcompatibledc
 
     Args:
+        settings (settings.Settings): flashy settings object
         name (str): name of the thread
         index_range (list): range of LED indices to read data from
         queue (queue.Queue): queue to add the values to
-        led_map (list): LED index to pixel coordinates mapping from the profile
     
     Attributes:
+        settings (settings.Settings): flashy settings object
         name (str): name of the thread
-        dc (str): hdc screen object
         index_range (list): range of LED indices to read data from
         queue (queue.Queue): queue to add the values to
-        led_map (list): LED index to pixel coordinates mapping from the profile
         index_order (list): list of indices in the index_range in a random order
         logger (Logger): logger object used to write logs from this thread
     """
     def __init__(self, settings, name=None, index_range=[], queue=None):
         super(ScreenReaderAsync, self).__init__()
         self.name = name
-        self.dc = windll.user32.GetDC(None)
         self.index_range = index_range
         self.queue = queue
         self.settings = settings
@@ -40,7 +38,7 @@ class ScreenReaderAsync(threading.Thread):
     def setup(self):
         """Set up variables for the run.
         """
-        self.frame_min_duration = 1.0 / self.settings.fps_limit  # seconds
+        self._frame_min_duration = 1.0 / self.settings.fps_limit  # seconds
         self.index_order = list(range(self.index_range[0], self.index_range[1]))
         random.shuffle(self.index_order)
 
@@ -50,11 +48,9 @@ class ScreenReaderAsync(threading.Thread):
         min_x = min(coord[0] for led_pixels in led_coord_lists for coord in led_pixels)
         max_y = max(coord[1] for led_pixels in led_coord_lists for coord in led_pixels)
         min_y = min(coord[1] for led_pixels in led_coord_lists for coord in led_pixels)
-        self.bbox = (min_x, min_y, max_x+1, max_y+1)
+        self._bbox = (min_x, min_y, max_x+1, max_y+1)
 
         # set up some helpful variables
-        self.size = (self.bbox[2]-self.bbox[0], self.bbox[3]-self.bbox[1])
-        self.position = (self.bbox[0], self.bbox[1])
         self.hwnd = None
 
     def run(self):
@@ -66,39 +62,20 @@ class ScreenReaderAsync(threading.Thread):
             # start a frame
             frame_start = datetime.utcnow()
 
-            try:
-                # get a screenshot using BitBlt
-                hdc = win32gui.GetWindowDC(self.hwnd)
-                dc = win32ui.CreateDCFromHandle(hdc)
-                memdc = dc.CreateCompatibleDC()
-                bitmap = win32ui.CreateBitmap()
-                bitmap.CreateCompatibleBitmap(dc, self.size[0], self.size[1])
-                memdc.SelectObject(bitmap)
-                memdc.BitBlt((0, 0), self.size, dc, self.position, win32con.SRCCOPY)
-                bits = bitmap.GetBitmapBits(True)
-                current_screenshot = Image.frombuffer('RGB', self.size, bits, 'raw', 'BGRX', 0, 1)
-            except Exception as e:
-                self.logger.error(f"Error getting a screenshot in ScreenReaderAsync.run(): {e}")
-                time.sleep(1)
-                continue
-            finally:
-                win32gui.DeleteObject(bitmap.GetHandle())
-                memdc.DeleteDC()
-                win32gui.ReleaseDC(self.hwnd, hdc)
-
-            # add the pixel values to a queue
-            for i in self.index_order:
-                item = self._mean_rgb([
-                    current_screenshot.getpixel((coords[0]-self.bbox[0], coords[1]-self.bbox[1]))
-                    for coords in self.settings.get_pixel_list(i)
-                ])
-                if item is not None:
-                    self.queue.put(i, item)
+            with BitBltScreenshot(bbox=self._bbox) as screenshot:
+                # add the pixel values to a queue
+                for i in self.index_order:
+                    item = self._mean_rgb([
+                        screenshot.getpixel((coords[0]-self._bbox[0], coords[1]-self._bbox[1]))
+                        for coords in self.settings.get_pixel_list(i)
+                    ])
+                    if item is not None:
+                        self.queue.put(i, item)
             
             # how long was this frame in milliseconds
             frame_duration = (datetime.utcnow() - frame_start).total_seconds()
             if frame_duration < 1e-5: frame_duration
-            time_left_in_frame = self.frame_min_duration - frame_duration
+            time_left_in_frame = self._frame_min_duration - frame_duration
             # make sure it waits for at least a millisecond
             # this miraculously saves some CPU usage
             if time_left_in_frame <= 0:
